bc68d935477e00464889b5f71af96cfd7267988a
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-04 21:54:52 +0800

    Create 3024. Type of Triangle.cpp

---
+++ 3024. Type of Triangle.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        int a = nums[0], b = nums[1], c = nums[2];
+        if (a + b <= c || b + c <= a || c + a <= b) return "none";
+        else if (a == b && b == c) return "equilateral";
+        else if (a == b || b == c || c == a) return "isosceles";
+        return "scalene";
+    }
+};
