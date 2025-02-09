47f96dc01a9e076c78b7dc791e378580fdd80d09
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-10 00:08:39 +0800

    Create 3184. Count Pairs That Form a Complete Day I.cpp

---
+++ 3184. Count Pairs That Form a Complete Day I.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCompleteDayPairs(vector<int>& hours) {
+        int ret = 0;
+        unordered_map<int, int> Hash;
+        for (auto h : hours)
+        {
+            ret += Hash[(24 - h%24) % 24];
+            Hash[h%24]++;
+        }  
+        return ret;
+    }
+};
