8f07b63dfc9e20b76fd3ee7d2a0240c7d32a0aa5
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-09 21:09:57 +0800

    Create 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum.cpp

---
+++ 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int prefix = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i-1]+1) break;
+            prefix += nums[i];
+        }
+        unordered_set<int> Hash;
+        for (auto &n : nums) Hash.insert(n);
+
+        while (Hash.count(prefix) != 0) prefix++;
+        
+        return prefix;
+    }
+};
