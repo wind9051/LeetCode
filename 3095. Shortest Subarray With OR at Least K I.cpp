2462e4f2396080340eb27c91d2bcb4b6fe38242b
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-14 03:30:40 +0800

    Create 3095. Shortest Subarray With OR at Least K I.cpp

---
+++ 3095. Shortest Subarray With OR at Least K I.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int temp = 0;
+        int minL = INT_MAX;
+        int L = 0, R = 0;
+        while (R < nums.size()) {
+            temp |= nums[R];
+            while (L <= R && temp >= k) {
+                minL = min(minL, R-L+1);
+                L++;
+
+                temp = 0;
+                for (int idx = L; idx <= R; idx++) {
+                    temp |= nums[idx];
+                }
+            }
+            R++;
+        }
+        if (minL == INT_MAX) return -1;
+        return minL;
+    }
+};
