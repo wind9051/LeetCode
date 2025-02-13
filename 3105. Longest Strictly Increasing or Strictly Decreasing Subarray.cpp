788d9079ed7f8b2bc49d73ee6692c34590e97081
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-14 03:44:44 +0800

    Create 3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp

---
+++ 3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int now = 0;
+        int maxL = 0, incL = 0, decL = 0;
+        for (auto n : nums) {
+            if (now < n) incL++, decL = 1;
+            else if (now > n) decL++, incL = 1;
+            else incL = decL = 1;
+
+            now = n;
+            maxL = max(maxL, max(incL, decL));
+        }
+        return maxL;
+    }
+};
