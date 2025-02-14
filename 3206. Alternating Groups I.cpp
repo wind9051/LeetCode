2a33bd949261274da036252c42730ae1a02928b9
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-15 03:50:08 +0800

    Create 3206. Alternating Groups I.cpp

---
+++ 3206. Alternating Groups I.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors) 
+    {
+        for (int i = 0; i < 2; i++) {
+            colors.push_back(colors[i]);
+        }
+                    
+        int ans = 0;
+        for (int i = 0; i < colors.size()-2; i++) {
+            if (colors[i] == colors[i+2] and colors[i] != colors[i+1]) ans++;
+        }
+        return ans;
+    }
+};
