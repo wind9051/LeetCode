e4e6b57f91cd1b65c049875dfaad6abc030d4562
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-17 04:54:00 +0800

    Create 2022. Convert 1D Array Into 2D Array.cpp

---
+++ 2022. Convert 1D Array Into 2D Array.cpp
@@ -0,0 +1,17 @@
+/*
+    Matrix
+*/
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if (original.size() != m*n) return {};
+
+        vector<vector<int>> ret(m, vector<int>(n));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ret[i][j] = original[i*n+j];
+            }
+        }
+        return ret;
+    }
+};
