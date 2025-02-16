c3ce52c10b9ef036d63ce8aa42758a120f48e9ec
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-17 05:27:33 +0800

    Create 1275. Find Winner on a Tic Tac Toe Game.cpp

---
+++ 1275. Find Winner on a Tic Tac Toe Game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string tictactoe(vector<vector<int>>& moves) {
+        vector<int> R(3, 0), C(3, 0);
+
+        int D = 0, AD = 0; //diag, antidiag
+        for (int i = 0; i < moves.size(); i++) {
+            int x = moves[i][0];
+            int y = moves[i][1];
+            if (i%2 == 0) {
+                R[x]++, C[y]++;
+                if (x == y) D++;
+                if (x == 2-y) AD++;
+            }
+            else {
+                R[x]--, C[y]--;
+                if (x == y) D--;
+                if (x == 2-y) AD--;
+            }
+            if (R[x] == 3  || C[y] == 3  || D == 3  || AD == 3) return "A";
+            if (R[x] == -3 || C[y] == -3 || D == -3 || AD == -3) return "B";
+        }
+        if (moves.size() == 9) return "Draw";
+        return "Pending";
+    }
+};
