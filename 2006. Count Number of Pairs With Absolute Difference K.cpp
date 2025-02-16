963e1aed049e9976c73642caee3c9128472a9d63
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-17 04:45:42 +0800

    Create 2006. Count Number of Pairs With Absolute Difference K.cpp

---
+++ 2006. Count Number of Pairs With Absolute Difference K.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int cnt = 0;
+        vector<int> dic(105, 0);
+        for (auto n : nums) dic[n]++;
+        
+        for (int i = k+1; i <= 100; i++) {
+            cnt += dic[i] * dic[i-k];
+        }
+        return cnt;
+    }
+};
