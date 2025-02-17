0e332ce80ee57e7cf5e101e0e26a2182e4cc57d3
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-17 14:28:12 +0800

    Create 1030. Matrix Cells in Distance Order.cpp

---
+++ 1030. Matrix Cells in Distance Order.cpp
@@ -0,0 +1,19 @@
+/*
+    Sort_Lambda 
+*/
+class Solution {
+public:
+    vector<vector<int>> allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        vector<vector<int>> ans;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ans.push_back({i, j});
+            }
+        }
+        int rC = rCenter, cC = cCenter;
+        sort (begin(ans), end(ans), [&](vector<int>& a, vector<int>& b) {
+            return (abs(rC - a[0]) + abs(cC - a[1])) < (abs(rC - b[0]) + abs(cC - b[1]));
+        });
+        return ans;
+    }
+};
