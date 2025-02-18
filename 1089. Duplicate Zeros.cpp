5d8222eb3f31e4168793de1aff8240952292e2bf
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-18 19:21:32 +0800

    Create 1089. Duplicate Zeros.cpp

---
+++ 1089. Duplicate Zeros.cpp
@@ -0,0 +1,20 @@
+/*
+    Two pointer
+*/
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int L = 0, R = 0, zero = 0;
+        while (L + zero < arr.size()) {
+            if (arr[L++] == 0) zero++;
+        }
+        L--;
+        R = L + zero;
+
+        while (L >= 0) {
+            if (R < arr.size()) arr[R] = arr[L];
+            if (arr[L] == 0 && --R) arr[R] = 0;
+            L--; R--;
+        }
+    }
+};
