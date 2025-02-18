9515a80649f7b0c1ef68829cffa9c1ace8600c32
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-18 20:05:20 +0800

    Create 1114. Print in Order.cpp
    Mutilthreading

---
+++ 1114. Print in Order.cpp
@@ -0,0 +1,31 @@
+//https://leetcode.cn/problems/print-in-order/solutions/445416/c-hu-chi-suo-tiao-jian-bian-liang-xin-hao-liang-yi/
+/*
+    multithreading
+*/
+#include <semaphore.h>
+
+class Foo {
+private:
+    sem_t sem_1, sem_2;
+
+public:
+    Foo() {
+        sem_init(&sem_1, 0, 0), sem_init(&sem_2, 0, 0);
+    }
+
+    void first(function<void()> printFirst) {
+        printFirst();
+        sem_post(&sem_1);
+    }
+
+    void second(function<void()> printSecond) {
+        sem_wait(&sem_1);
+        printSecond();
+        sem_post(&sem_2);
+    }
+
+    void third(function<void()> printThird) {
+        sem_wait(&sem_2);
+        printThird();
+    }
+};
