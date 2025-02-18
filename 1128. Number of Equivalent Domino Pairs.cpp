712e1ce62a6ed70bb4eeb8f94810ff1b648a6614
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-18 20:11:48 +0800

    Create 1128. Number of Equivalent Domino Pairs.cpp

---
+++ 1128. Number of Equivalent Domino Pairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        map<pair<int, int>, int> hash;
+        int ret = 0;
+        for (auto & v : dominoes) {
+            if (v[0] > v[1]) swap(v[0], v[1]);
+            ret += hash[{v[0], v[1]}]++;
+        }
+        return ret;
+    }
+};
