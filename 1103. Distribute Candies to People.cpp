fe5e14346230ac5db119c758d06c5dbf5dc34606
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-18 19:56:42 +0800

    Update 1103. Distribute Candies to People.cpp

--- 1103. Distribute Candies to People.cpp
+++ 1103. Distribute Candies to People.cpp
@@ -6,23 +6,24 @@ class Solution {
 public:
     vector<int> distributeCandies(int candies, int n) {
         /*
-            1+2+3+...+n = n(n+1)/2 <= C(candies)
+            P = people, C = candies
+
+            1+2+3+...+n = n(n+1)/2 <= C
             n^2 + n - 2C = 0
             n = (-1 +-sqrt(1+8C))/2
 
-            (i+1) + (n+i+1) + (2n+i+1) +...+ (R-1)n+i+1 = k(k-1)/2n + k(i+1)
+            (i+1) + (P+i+1) + (2P+i+1) +...+ ((R-1)P+i+1) = R(R-1)/2*P + R(i+1)
         */
-        int m = (-1 + sqrt(1 + 8.0 * candies) ) / 2;
+        int n = (-1 + sqrt(1 + 8.0 * C) ) / 2;
         //Round, Extra
         int R = m / n, E = m % n;
-        cout<<R<<" "<<E<<" "<<m<<endl;
 
-        vector<int> ret(n, 0);
-        for (int i = 0; i < n; i++) {
-            ret[i] = R*(R-1)/2*n + R*(i+1); //R-1 Round
-            if (i < E) ret[i] += R * n + i + 1; //R Round
+        vector<int> ret(P, 0);
+        for (int i = 0; i < P; i++) {
+            ret[i] = R*(R-1)/2*P + R*(i+1); //R-1 Round
+            if (i < E) ret[i] += R * P + i + 1; //R Round
         }
-        ret[E] += candies - m * (m + 1) / 2;
+        ret[E] += C - n * (n + 1) / 2;
         return ret;
     }
 };
