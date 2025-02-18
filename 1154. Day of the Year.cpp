f23a9a60acda946d433f50b54954a59bf750a86d
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-18 20:54:25 +0800

    Create 1154. Day of the Year.cpp

---
+++ 1154. Day of the Year.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int is_leap(int y) {
+        return y % 4 == 0 && y % 100 != 0 || y % 400 == 0;
+    }
+    
+    int dayOfYear(string date) {
+        vector<int> days = {31,28,31,30,31,30,31,31,30,31,30,31};
+        int y = stoi(date.substr(0, 4));
+        int m = stoi(date.substr(5, 2));
+        int d = stoi(date.substr(8, 2));
+
+        if (is_leap(y)) days[1] ++;
+
+        int ans = 0;
+        for (int i = 0; i < m - 1; i++) {
+            ans += days[i];
+        }
+        ans += d;
+
+        return ans;
+    }
+};
