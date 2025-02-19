5482b7c028041e9700628fb0148b68650d22f8a8
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-19 10:05:22 +0800

    Create 1995. Count Special Quadruplets.cpp

---
+++ 1995. Count Special Quadruplets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    //a+b+c=d
+    int countQuadruplets(vector<int>& nums) {
+        int cnt = 0;
+        unordered_map<int, int> hash;
+        for (int i = 1; i < nums.size(); i++) {
+            //nums[a] + nums[b]
+            for (int j = 0; j < i; j++) {
+                hash[nums[i]+nums[j]]++;
+            }
+            
+            //nums[d] - nums[c] 
+            for (int k = i+2; k < nums.size(); k++){
+                cnt += hash[nums[k]-nums[i+1]];
+            }
+        }
+        return cnt;
+    }
+};
