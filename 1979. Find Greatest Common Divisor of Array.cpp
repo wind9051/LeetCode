4f602082f2a30b0850aaf84c5c361e1760de796c
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-20 06:13:11 +0800

    Create 1979. Find Greatest Common Divisor of Array.cpp

---
+++ 1979. Find Greatest Common Divisor of Array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        int x = *min_element(nums.begin(), nums.end());
+        int y = *max_element(nums.begin(), nums.end());
+        return __gcd(x, y);
+    }
+};
