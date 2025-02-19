745a158da7483d8f50695f133a03f4cc7470111c
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-20 05:41:28 +0800

    Create 2652. Sum Multiples.cpp

---
+++ 2652. Sum Multiples.cpp
@@ -0,0 +1,17 @@
+/*
+    Math
+    S = k + 2k + ... + mk
+      = k × (1 + 2 + ... + m)
+      = k x m(m+1)/2
+*/
+
+class Solution {
+public:
+    //Arithmetic Sequence
+    int cal(int n, int k) {
+        return k * (n/k) * (n/k + 1) / 2;;
+    }
+    int sumOfMultiples(int n) {
+        return cal(n, 3) + cal(n, 5) + cal(n, 7) - cal(n, 3*5) - cal(n, 5*7) - cal(n, 3*7) + cal(n, 3*5*7);
+    }
+};
