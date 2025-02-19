b5393a1095ad4a654065beaa5a3ee1708208c0d4
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-20 05:54:20 +0800

    Create 197. Rising Temperature.sql

---
+++ 197. Rising Temperature.sql
@@ -0,0 +1,10 @@
+/*
+    Mysql
+*/
+# Write your MySQL query statement below
+SELECT w2.id 
+FROM Weather w1, Weather w2
+WHERE 
+    DATEDIFF(w2.recordDate, w1.recordDate) = 1 
+AND 
+    w2.temperature > w1.temperature;
