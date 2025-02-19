9b9591faca3682a7490d34b6c75fe51e0ad4030f
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-19 13:09:56 +0800

    Create 1436. Destination City.cpp

---
+++ 1436. Destination City.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_set<string> hash;
+        for (auto city : paths) hash.insert(city[0]);
+        
+        for (auto city : paths) {
+            if (!hash.count(city[1])) return city[1];
+        }
+        
+        return "";
+    }
+};
