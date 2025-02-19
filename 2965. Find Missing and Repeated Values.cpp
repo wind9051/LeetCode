6474e9b51c3c84ed2a9f52361662befd033a3439
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-20 04:30:35 +0800

    Create 2965. Find Missing and Repeated Values.cpp

---
+++ 2965. Find Missing and Repeated Values.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        
+        vector<int> Freq(n*n+5, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Freq[grid[i][j]]++;
+            }
+        }
+
+        vector<int> ans(2, 0);
+        for (int i = 1; i <= n*n; i++) {
+            if (Freq[i] == 2) ans[0] = i;
+            if (Freq[i] == 0) ans[1] = i;
+        }
+        return ans;
+    }
+};
