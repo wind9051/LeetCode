80033fede5f8e64158ffe5cad4579e3424b4aa6b
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-22 06:00:14 +0800

    Create 303. Range Sum Query - Immutable.cpp

---
+++ 303. Range Sum Query - Immutable.cpp
@@ -0,0 +1,20 @@
+class NumArray {
+public:
+    NumArray(vector<int> &nums) {
+        accu.push_back(0);
+        for (int num : nums)
+            accu.push_back(accu.back() + num);
+    }
+
+    int sumRange(int i, int j) {
+        return accu[j + 1] - accu[i];
+    }
+private:
+    vector<int> accu;
+};
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.sumRange(1, 2); 
