62e9249180a2c30cec1ab1263ea6309816395316
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-22 06:04:07 +0800

    Create 409. Longest Palindrome.cpp

---
+++ 409. Longest Palindrome.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> hash;
+        for (auto ch : s) hash[ch]++;
+
+        int len = 0, odd = 0;
+        for (auto [c, n] : hash) {
+            if (n % 2) odd = 1;
+            len += n / 2 * 2;
+        }
+        return len + odd;
+    }
+};
