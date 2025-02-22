33824ab58a28dd03e112e999ff86b88bdc8b08e1
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-23 00:31:24 +0800

    Create 2678. Number of Senior Citizens.cpp

---
+++ 2678. Number of Senior Citizens.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int cnt = 0;
+        for (auto s : details) {
+            int age = (s[11]-'0') * 10+ (s[12]-'0');
+            if (age > 60) cnt++;
+        }
+        return cnt;
+    }
+};
