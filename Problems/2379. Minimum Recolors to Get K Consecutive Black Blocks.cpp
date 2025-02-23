004cce1d1049a433d58ba0a4d8cf2fde5f758627
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-23 09:30:23 +0800

    Create 2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp

---
+++ Problems/2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int L = 0, R = 0, cnt_w = 0, ans = INT_MAX;
+        while (R < blocks.size()) {
+            if (blocks[R] == 'W') cnt_w++;
+            if (R - L + 1 == k) {
+                ans = min(ans, cnt_w);
+                if (blocks[L++] == 'W') cnt_w--;
+            }
+            R++;
+        }
+        return ans;
+    }
+};
