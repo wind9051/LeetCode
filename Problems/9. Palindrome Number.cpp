647499300dd5a97c9babd77f780c1935a171e8e2
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-24 04:34:55 +0800

    Create 9. Palindrome Number.cpp

---
+++ Problems/9. Palindrome Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        string s = to_string(x);
+        int L = 0, R = s.size()-1;
+        while (L < R) {
+            if (s[L++] != s[R--]) return false;
+        }
+        return true;
+    }
+};
