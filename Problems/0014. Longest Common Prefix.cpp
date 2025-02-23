57bd547b15102f545793ebc15a45bca38a3c4bc8
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-24 04:38:48 +0800

    Create 0014. Longest Common Prefix.cpp

---
+++ Problems/0014. Longest Common Prefix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string str = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            int now = 0;
+            while (now < str.size() && now < strs[i].size()) {
+                if (str[now] != strs[i][now]) break;
+                now ++;
+            }
+            str = str.substr(0, now);
+        }
+        return str;
+    }
+};
