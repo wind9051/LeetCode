5bd53f97c18e396b32aa7c7537787b8e9f00c589
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-23 09:34:58 +0800

    Create 2418. Sort the People.cpp

---
+++ Problems/2418. Sort the People.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    static bool cmp (int & a, int & b) {
+        return a > b;
+    }
+
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int n = names.size();
+
+        unordered_map<int, string> Hash;
+
+        for (int i = 0; i < n; i++) {
+            Hash[heights[i]] = names[i];
+        }
+
+        sort(heights.begin(), heights.end(), cmp);
+
+        vector<string> sorted_names(n);
+        for (int i = 0; i < n; i++) {
+            sorted_names[i] = Hash[heights[i]];
+        }
+
+        return sorted_names;
+    }
+};
