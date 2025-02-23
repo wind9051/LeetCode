e3a6d059f4ac9d2ffe487ac6b201cf4f134f3e7d
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-24 04:43:19 +0800

    Create 20. Valid Parentheses.cpp

---
+++ Problems/20. Valid Parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (auto ch : s) {
+            if (ch == '(' || ch == '[' || ch == '{') stk.push(ch);
+            else if (!stk.empty()) {
+                if (stk.top() == '(' && ch == ')') stk.pop();
+                else if (stk.top() == '{' && ch == '}') stk.pop();
+                else if (stk.top() == '[' && ch == ']') stk.pop();
+                else return false;
+            }
+            else return false;
+        }
+        return stk.empty();
+    }
+};
