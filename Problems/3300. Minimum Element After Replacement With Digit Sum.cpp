4143263ff1a614898f574b85049c777d313de469
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-25 06:32:49 +0800

    Create 3300. Minimum Element After Replacement With Digit Sum.cpp

---
+++ Problems/3300. Minimum Element After Replacement With Digit Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int minn = 1e4;
+        for (auto n : nums) {
+            int now = n, sum = 0;
+            while (now) {
+                sum += now % 10;
+                now /= 10;
+            }
+            minn = min(minn, sum);
+        }
+        return minn;
+    }
+};
