b12294be7920dbcd6e9e72b3699504b5d05b1c92
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-25 07:42:34 +0800

    Create 1394. Find Lucky Integer in an Array.cpp

---
+++ Problems/1394. Find Lucky Integer in an Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < arr.size(); i++) {
+            hash[arr[i]]++;
+        }
+        int maxn = -1;
+        for (auto [k, v] : hash) {
+            if (k == v) maxn = max(maxn, v);
+        }
+        return maxn;
+    }
+};
