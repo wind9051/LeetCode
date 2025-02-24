4cb8c58fe0e4f9f28732b6de404832eca9c67275
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-25 06:49:13 +0800

    Create 1232. Check If It Is a Straight Line.cpp

---
+++ Problems/1232. Check If It Is a Straight Line.cpp
@@ -0,0 +1,14 @@
+/*
+    math, 斜率
+*/
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int x0 = coordinates[0][0], dx = coordinates[1][0] - coordinates[0][0];
+        int y0 = coordinates[0][1] , dy = coordinates[1][1] - coordinates[0][1];
+        for (auto i = 1; i < coordinates.size(); i++) {
+            if (dx * (coordinates[i][1] - y0) != (coordinates[i][0] - x0) * dy) return false;
+        }
+        return true;
+    }
+};
