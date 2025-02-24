eaac35c18216fac490932dc4127cf6363be854e3
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-25 06:10:29 +0800

    Create 3232. Find if Digit Game Can Be Won.cpp

---
+++ Problems/3232. Find if Digit Game Can Be Won.cpp
@@ -0,0 +1,14 @@
+/*
+  math
+*/
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int single_sum = 0, double_sum = 0;
+        for (auto n : nums) {
+            if (n < 10) single_sum += n;
+            else double_sum += n;
+        }
+        return single_sum != double_sum;
+    }
+};
