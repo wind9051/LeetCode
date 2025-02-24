883106f14d0bb8b0c42fc2bd68515271ff5dff15
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-25 07:39:23 +0800

    Create 1385. Find the Distance Value Between Two Arrays.cpp

---
+++ Problems/1385. Find the Distance Value Between Two Arrays.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool bs(vector<int>& num, int & L, int & R) {
+        int l = 0, r = num.size()-1;
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            //num−d <= y <= num+d
+            if (num[m] >= L && num[m] <= R) return 1;
+            if (num[m] < L) l = m+1;
+            if (num[m] > R) r = m-1;
+        }
+        return 0;
+    }
+    
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        sort(arr2.begin(), arr2.end());
+        
+        int cnt = 0;
+        for (int & n : arr1) {
+            int L = n - d;
+            int R = n + d;
+            if (!bs(arr2, L, R)) cnt++;
+        }
+        return cnt;
+    }
+};
+
+//num−d<=y<=num+d
