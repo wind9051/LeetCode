f260470cdbddc574fce06f7740c1aced76f1cb05
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-27 02:58:54 +0800

    Update and rename 0001. Two Sum.cpp to 0001. Two Sum_2.cpp

--- Problems/0001. Two Sum_2.cpp
+++ Problems/0001. Two Sum_2.cpp
@@ -1,9 +1,13 @@
+/*
+    O(n)
+*/
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> hash;
         for (int i = 0; i < nums.size(); i++) {
             int x = target - nums[i];
+            //if(hash.find(x) != hash.end())
             if (hash.count(x))
                 return {hash[x], i};
             hash[nums[i]] = i;
