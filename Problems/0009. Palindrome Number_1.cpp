7c8330fb6725667d331a53e6c72e3846d1bb6f90
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-27 03:25:04 +0800

    Update and rename 0009. Palindrome Number.cpp to 0009. Palindrome Number_1.cpp

--- Problems/0009. Palindrome Number_1.cpp
+++ Problems/0009. Palindrome Number_1.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0) return false;
+        
         string s = to_string(x);
         int L = 0, R = s.size()-1;
         while (L < R) {
@@ -10,3 +11,6 @@ public:
         return true;
     }
 };
+/*
+    Palindrome, 回文
+*/
