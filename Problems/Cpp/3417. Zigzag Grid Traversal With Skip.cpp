2f166c5828ef5c1aed6864238b0c90bb42f80788
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-28 02:34:02 +0800

    Update and rename 3417. Zigzag Grid Traversal With Skip.cpp to Problems/Cpp/3417. Zigzag Grid Traversal With Skip.cpp

--- Problems/Cpp/3417. Zigzag Grid Traversal With Skip.cpp
+++ Problems/Cpp/3417. Zigzag Grid Traversal With Skip.cpp
@@ -8,18 +8,19 @@ public:
         vector<int> ret;
         for (int i = 0; i < n; i++) {
             if (i%2 == 0) {
-                for (int j = 0;j < m; j++) {
+                for (int j = 0, cnt = 0; j < m; j++, L++) {
                     if (L%2 == 0) ret.push_back(grid[i][j]);
-                    L++;
                 }
             }
             else {
-                for (int j = m-1; j >= 0; j--) {
+                for (int j = m-1; j >= 0; j--, L++) {
                     if (L%2 == 0) ret.push_back(grid[i][j]);
-                    L++;
                 }
             }
         }
         return ret;
     }
 };
+/*
+    Matrix, 矩陣
+*/
