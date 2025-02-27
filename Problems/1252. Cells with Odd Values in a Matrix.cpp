0e38a44f5fa59c8799c9179c871660d3d4814a9f
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-02-27 13:24:53 +0800

    Update 1252. Cells with Odd Values in a Matrix.cpp

--- Problems/1252. Cells with Odd Values in a Matrix.cpp
+++ Problems/1252. Cells with Odd Values in a Matrix.cpp
@@ -1,17 +1,17 @@
-/*
-    matrix, math
-*/
 class Solution {
 public:
     int oddCells(int m, int n, vector<vector<int>>& indices) {
-        vector<int> M(m, 0), N(n, 0);
+        vector<int> row(m, 0), col(n, 0);
         for (auto i = 0; i < indices.size(); i++) {
-            M[indices[i][0]]++;
-            N[indices[i][1]]++;
+            row[indices[i][0]]++;
+            col[indices[i][1]]++;
         }
         int M_odd = 0, N_odd = 0;
-        for (auto i = 0; i < M.size(); i++) M_odd += M[i] % 2 ? 1 : 0;
-        for (auto i = 0; i < N.size(); i++) N_odd += N[i] % 2 ? 1 : 0;
+        for (auto i = 0; i < row.size(); i++) M_odd += row[i] % 2 ? 1 : 0;
+        for (auto i = 0; i < col.size(); i++) N_odd += col[i] % 2 ? 1 : 0;
         return M_odd * (n - N_odd) + N_odd * (m - M_odd);
     }
 };
+/*
+    matrix, math, 數學, 矩陣, 奇偶
+*/
