6e78d6648a306033e612d8706a0ad1e36fca5bb4
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-03-03 08:55:01 +0800

    Create 1863. Sum of All Subset XOR Totals.cpp

---
+++ Problems/Cpp/1863. Sum of All Subset XOR Totals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int sum = 0;
+    void dfs(int n, int L, int XOR, vector<int> & nums) {
+        if (n == L) {
+            sum += XOR;
+            return ;
+        }
+        dfs(n+1, L, XOR, nums);
+        dfs(n+1, L, XOR^nums[n], nums);
+    }
+    
+    int subsetXORSum(vector<int>& nums) {
+        sum = 0;
+        dfs(0, nums.size(), 0, nums);
+        return sum;
+    }
+    
+};
+/*
+    DFS, Subset, 子集
+*/
