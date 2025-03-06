93e4d7cfcece2567737c6e80ddd53ac711945e2f
JiaCheng <83905233+wind9051@users.noreply.github.com>
2025-03-06 15:52:25 +0800

    Update and rename 3131. Find the Integer Added to Array I.cpp to Problems/Cpp/3131. Find the Integer Added to Array I.cpp

--- Problems/Cpp/3131. Find the Integer Added to Array I.cpp
+++ Problems/Cpp/3131. Find the Integer Added to Array I.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int addedInteger(vector<int>& n1, vector<int>& n2) {
+        //return (accumulate(begin(n2), end(n2), 0) - accumulate(begin(n1), end(n1), 0)) / (int)n1.size();
         return *min_element(begin(n2), end(n2)) - *min_element(begin(n1), end(n1));
     }
 };
+/*
+    Math, 數學
+*/
